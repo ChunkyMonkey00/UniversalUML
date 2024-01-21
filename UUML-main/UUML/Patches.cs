679751bfb120e410688afdbc6144489156aec9b8
ChunkyMonkey00 <148571454+ChunkyMonkey00@users.noreply.github.com>
2024-01-21 17:51:09 -0500

    Add files via upload
    fixed several mistakes lol

---
+++ UUML-main/UUML/Patches.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using UnityEngine.UI;
+using System.Reflection.Emit;
+using Debug = UnityEngine.Debug;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Screen = UnityEngine.Screen;
+
+namespace UUML
+{
+    //Here is a template harmony patch :)
+    /*
+    [HarmonyPatch(typeof(ClassName), "MethodName")]
+    public class PlayerControllerB_Start_Patch
+    {
+        private static bool Prefix()
+        {
+            return true;
+        }
+    }
+    */
+}
---
+++ UUML-main/UUML/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("UUML")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UUML")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("38c8b141-fcec-41be-aba1-39aea05c306f")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
